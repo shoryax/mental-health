14036033c6b4495408b89ff2973ee17ed439dcba
shoryax <shoryavardhan7@gmail.com>
2025-07-20 23:49:10 +0530

    added

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -12,7 +12,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
 
-
 const Settings = () => {
   const [dailyReminders, setDailyReminders] = useState(true);
 
